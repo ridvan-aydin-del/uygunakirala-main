dd040be126860ca7175edce857076f4ddb6ca167
Rıdvan AYDIN <aydinridvan2834@gmail.com>
2022-10-14 16:33:17 +0300

    iconlar masaüstü-mobil bitti

--- app.js
+++ app.js
@@ -1,11 +1,36 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 10,
+var swiper = new Swiper(".mySwiper", { 
     spaceBetween: 0,
-    freeMode: true,
+    centeredSlides: false,
     breakpoints: {
-        700: {
-          slidesPerView: 5,
-          spaceBetween: 0
+        0: {
+          slidesPerView: 1,
+          
         },
-      }
-  });
+        700:{
+            slidesPerView: 5,
+        },
+        991:{
+            slidesPerView: 10,
+        },
+      },
+       
+    
+  }); 
+  var swiper1 = new Swiper1(".mySwiper s1", { 
+    spaceBetween: 0,
+    centeredSlides: false,
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+          
+        },
+        700:{
+            slidesPerView: 2,
+        },
+        991:{
+            slidesPerView: 4,
+        },
+      },
+       
+    
+  }); 
---
+++ appmobile.js
@@ -0,0 +1,18 @@
+var swiper1 = new Swiper1(".mySwiper1", { 
+    spaceBetween: 0,
+    centeredSlides: false,
+    breakpoints: {
+        0: {
+          slidesPerView: 2,
+          
+        },
+        700:{
+            slidesPerView: 2,
+        },
+        991:{
+            slidesPerView: 2,
+        },
+      },
+       
+    
+  }); 
