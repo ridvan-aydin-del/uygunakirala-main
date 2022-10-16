7a28c1ba0b31086a7a859f65240bf9ce95db2d13
Rıdvan AYDIN <aydinridvan2834@gmail.com>
2022-10-16 21:01:23 +0300

    slider

--- app.js
+++ app.js
@@ -2,35 +2,15 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 0,
     centeredSlides: false,
     breakpoints: {
-        0: {
-          slidesPerView: 1,
+      0: {
+        slidesPerView: 1,
           
-        },
-        700:{
-            slidesPerView: 5,
-        },
-        991:{
-            slidesPerView: 10,
-        },
       },
-       
-    
-  }); 
-  var swiper1 = new Swiper1(".mySwiper s1", { 
-    spaceBetween: 0,
-    centeredSlides: false,
-    breakpoints: {
-        0: {
-          slidesPerView: 1,
-          
-        },
-        700:{
-            slidesPerView: 2,
-        },
-        991:{
-            slidesPerView: 4,
-        },
+      700:{
+        slidesPerView: 5,
       },
-       
-    
+      991:{
+        slidesPerView: 10,
+      },
+      }, 
   }); 
--- appmobile.js
+++ appmobile.js
@@ -1,18 +1,16 @@
-var swiper1 = new Swiper1(".mySwiper1", { 
+var swiper = new Swiper(".mySwiper1", { 
     spaceBetween: 0,
     centeredSlides: false,
     breakpoints: {
-        0: {
-          slidesPerView: 2,
+      0: {
+        slidesPerView: 1,
           
-        },
-        700:{
-            slidesPerView: 2,
-        },
-        991:{
-            slidesPerView: 2,
-        },
       },
-       
-    
+      700:{
+        slidesPerView: 2,
+      },
+      991:{
+        slidesPerView: 4,
+      },
+      }, 
   }); 
